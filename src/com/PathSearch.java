9550173e5c2eac40ec3023f02c23a87300ffc9e0
Jay Nagle <naglejay@gmail.com>
2016-01-23 21:19:49 -0500

    Added primitive read logic for input file

--- src/com/PathSearch.java
+++ src/com/PathSearch.java
@@ -3,6 +3,10 @@
  */
 package com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author hp
  *
@@ -13,7 +17,27 @@ public class PathSearch {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		new PathSearch().readInputFile();
 	}
 
+	public void readInputFile() {
+		BufferedReader br = null;
+		try {
+			String line;
+			br = new BufferedReader(new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream("input.txt")));
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 }
